bb4220713df413323a1f2ee67da9a86a962cc0a7
Daniel <danielaraujos@live.com>
2016-06-09 23:38:36 -0300

    Criando uma função de backup - Tela

--- src/com/spej/view/BackupGerar.java
+++ src/com/spej/view/BackupGerar.java
@@ -5,6 +5,8 @@
  */
 package com.spej.view;
 
+import java.awt.Color;
+
 /**
  *
  * @author Daniel
@@ -18,7 +20,26 @@ public class BackupGerar extends javax.swing.JDialog {
         super(parent, modal);
         initComponents();
         setLocationRelativeTo( null ); // Centralizar a tela no meio
+        getContentPane().setBackground(new Color(238,238,238));
+        Escrita();
+        FuncaoBackup();
+        
     }
+    
+    public void Escrita(){
+        texto.setText("Para sua segurança faça um backup semanal de seus dados!  É de suma importancia.");
+    }
+    
+    public void FuncaoBackup(){
+        try {
+            //Conecta ao banco de dados 
+            JFC_Salvar_Backup.setVisible(false);
+        } catch (Exception e) {
+            Mensagem.erro(this, e);
+        }  
+    }
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -29,21 +50,92 @@ public class BackupGerar extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanelGerenciarUsuario = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        texto = new javax.swing.JLabel();
+        jBotaoGerar = new javax.swing.JButton();
+        JFC_Salvar_Backup = new javax.swing.JFileChooser();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jPanelGerenciarUsuario.setBackground(new java.awt.Color(238, 238, 238));
+        jPanelGerenciarUsuario.setToolTipText("Gerenciar Usuários");
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("Realizar Backup");
+
+        jBotaoGerar.setText("Gerar backup");
+        jBotaoGerar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jBotaoGerarMouseClicked(evt);
+            }
+        });
+        jBotaoGerar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBotaoGerarActionPerformed(evt);
+            }
+        });
+
+        JFC_Salvar_Backup.setDialogType(javax.swing.JFileChooser.SAVE_DIALOG);
+        JFC_Salvar_Backup.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        javax.swing.GroupLayout jPanelGerenciarUsuarioLayout = new javax.swing.GroupLayout(jPanelGerenciarUsuario);
+        jPanelGerenciarUsuario.setLayout(jPanelGerenciarUsuarioLayout);
+        jPanelGerenciarUsuarioLayout.setHorizontalGroup(
+            jPanelGerenciarUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelGerenciarUsuarioLayout.createSequentialGroup()
+                .addGroup(jPanelGerenciarUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanelGerenciarUsuarioLayout.createSequentialGroup()
+                        .addGap(67, 67, 67)
+                        .addComponent(texto, javax.swing.GroupLayout.PREFERRED_SIZE, 551, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanelGerenciarUsuarioLayout.createSequentialGroup()
+                        .addGap(55, 55, 55)
+                        .addComponent(jBotaoGerar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(JFC_Salvar_Backup, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanelGerenciarUsuarioLayout.createSequentialGroup()
+                        .addGap(289, 289, 289)
+                        .addComponent(jLabel1)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanelGerenciarUsuarioLayout.setVerticalGroup(
+            jPanelGerenciarUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelGerenciarUsuarioLayout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(texto, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanelGerenciarUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jBotaoGerar)
+                    .addComponent(JFC_Salvar_Backup, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanelGerenciarUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanelGerenciarUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jBotaoGerarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jBotaoGerarMouseClicked
+        JFC_Salvar_Backup.setVisible(true);
+    }//GEN-LAST:event_jBotaoGerarMouseClicked
+
+    private void jBotaoGerarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBotaoGerarActionPerformed
+        
+    }//GEN-LAST:event_jBotaoGerarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -88,5 +180,10 @@ public class BackupGerar extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JFileChooser JFC_Salvar_Backup;
+    private javax.swing.JButton jBotaoGerar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanelGerenciarUsuario;
+    private javax.swing.JLabel texto;
     // End of variables declaration//GEN-END:variables
 }
